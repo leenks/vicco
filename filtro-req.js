210f8af0cfeb66839746f93f6f1427d265579c4c
Aline Espalaor <42449926+leenks@users.noreply.github.com>
2024-03-04 08:56:47 -0300

    voltando filtro antigo e subindo filtro-req

--- filtro-req.js
+++ filtro-req.js
@@ -17,8 +17,8 @@ window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = 'none';
     }
-    
 }
+
 // Adiciona o filtro à lista de filtros selecionados
 function adicionarFiltro(nomeFiltro) {
     const filtrosSelecionados = document.querySelector('.filtros-selecionados');
@@ -26,47 +26,47 @@ function adicionarFiltro(nomeFiltro) {
 
 }
 
-
 // CARDS MODOS DE LISTAGEM
 let modoExibicao = 'card';
 
 function alterarModo() {
     if (modoExibicao === 'card') {
-        modoExibicao = 'lista';
-        atualizarModoExibicao();
-    } else if (modoExibicao === 'lista') {
-        modoExibicao = 'simples';
-        atualizarModoExibicao();
+        modoExibicao = 'modo-simples';
+    } else if (modoExibicao === 'modo-simples') {
+        modoExibicao = 'modo-simples2'; // Desabilitando temporariamente o modo-simples2
     } else {
         modoExibicao = 'card';
-        atualizarModoExibicao();
     }
+    atualizarModoExibicao();
 }
 
 function atualizarModoExibicao() {
     const cardsContainer = document.querySelector('.cards-container');
+    const iconElement = document.getElementById('modo-icon');
+    const conteudoModoSimples2 = document.getElementById('conteudoModoSimples2');
+    const conteudoModoSimples = document.querySelector('.conteudo-in');
 
     // Remove todas as classes de modo
-    cardsContainer.classList.remove('modo-lista', 'modo-simples');
+    cardsContainer.classList.remove('modo-simples2', 'modo-simples');
 
     // Adiciona a classe do modo atual
-    if (modoExibicao === 'lista') {
-        cardsContainer.classList.add('modo-lista');
-    } else if (modoExibicao === 'simples') {
-        cardsContainer.classList.add('modo-simples');
-    }   
-    
-
-    // Pega a referência do ícone
-    const iconElement = document.getElementById('modo-icon');
-
-    // Alterna entre os ícones de lista e large no mobile
-    if (modoExibicao === 'lista') {
+    if (modoExibicao === 'modo-simples2') {
+        cardsContainer.classList.add('modo-simples2');
         iconElement.classList.remove('fa-th-large');
         iconElement.classList.add('fa-th-list');
-    } else {
+        conteudoModoSimples2.style.display = 'block'; // Exibe a tabela
+        
+        
+    } else if (modoExibicao === 'modo-simples') {
+        cardsContainer.classList.add('modo-simples');
         iconElement.classList.remove('fa-th-list');
         iconElement.classList.add('fa-th-large');
+        conteudoModoSimples2.style.display = 'none'; // Oculta a tabela
+        conteudoModoSimples.style.display = 'block'; // Exibe o conteúdo abaixo da tabela
+    } else {
+        cardsContainer.classList.remove('modo-simples', 'modo-simples2'); // Remover outras classes se estiverem presentes
+        iconElement.classList.remove('fa-th-list', 'fa-th-large'); // Remover outras classes se estiverem presentes
+        conteudoModoSimples2.style.display = 'none'; // Ocultar a tabela
+        conteudoModoSimples.style.display = 'block'; // Exibir o conteúdo abaixo da tabela
     }
 }
-
