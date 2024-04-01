6f2180580e12caef608ba48f37b0f4a03871cfa9
Aline Espalaor <42449926+leenks@users.noreply.github.com>
2024-04-01 18:47:14 -0300

    modos de visu

--- filtro-cad-gr-prod.js
+++ filtro-cad-gr-prod.js
@@ -1,36 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Oculta a tabela assim que o DOM estiver pronto
-    var tabela = document.getElementById('tabelaGrupos');
-    tabela.style.display = 'none';
+    var tabelaContainer = document.getElementById('tabelaContainer');
+    tabelaContainer.style.display = 'none'; // Garante que a tabela esteja oculta inicialmente
 
     // Define o modo de exibição inicial
     var modoExibicao = 'cards';
 });
 
 function alterarModo() {
-    // Obtém referências aos elementos
-    var cards = document.querySelectorAll('.card');
-    var tabela = document.getElementById('tabelaGrupos');
+    // Obtém referência ao contêiner da tabela
+    var tabelaContainer = document.getElementById('tabelaContainer');
+    // Obtém referências aos cards
+    var cardsContainer = document.querySelector('.container');
+
+    // Obtém referência ao botão de visualização
     var visualizacaoBtn = document.getElementById('visualizacaoBtn');
 
     // Verifica se a tabela está visível
-    if (tabela.style.display === 'none') {
-        // Mostra a tabela
-        tabela.style.display = 'block';
-        // Oculta os cards
-        cards.forEach(function(card) {
-            card.style.display = 'none';
-        });
-        // Altera o ícone do botão de visualização para lista
+    if (tabelaContainer.style.display === 'none') {
+        // Mostra a tabela e oculta os cards
+        tabelaContainer.style.display = 'block';
+        cardsContainer.style.display = 'none';
+
+        // Altera o texto do botão de visualização
         visualizacaoBtn.innerHTML = '<span class="fas fa-th-list"></span> &nbsp; VISUALIZAÇÃO';
     } else {
-        // Oculta a tabela
-        tabela.style.display = 'none';
-        // Mostra os cards
-        cards.forEach(function(card) {
-            card.style.display = 'block';
-        });
-        // Altera o ícone do botão de visualização para large
+        // Oculta a tabela e mostra os cards
+        tabelaContainer.style.display = 'none';
+        cardsContainer.style.display = 'block';
+
+        // Altera o texto do botão de visualização
         visualizacaoBtn.innerHTML = '<span class="fas fa-th-large"></span> &nbsp; VISUALIZAÇÃO';
     }  
 }
